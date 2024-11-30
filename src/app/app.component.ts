2a626ff909f46d2ee42a4225ef95bff6fe96d1b9
manuelborbua <mborbua23@gmail.com>
2024-11-30 13:53:19 -0500

    Inicialización del proyecto, se crearon los componentes de Header y Footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  template: '<router-outlet></router-outlet><app-header></app-header><app-footer></app-footer>',
+  styles: ''
 })
 export class AppComponent {
   title = 'd-and-d-wiki';
