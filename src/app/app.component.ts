11ead5c73f817ba6fb4f641aec00feae68647b2a
manuelborbua <mborbua23@gmail.com>
2024-12-02 13:34:28 -0500

    Se crearon los componentes del banner y video, también se agregó la sección principal y de noticias.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { VideoSectionComponent } from './video-section/video-section.component';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  template: '<router-outlet></router-outlet><app-header></app-header><app-footer></app-footer>',
+  imports: [RouterOutlet, HeaderComponent, BannerComponent, VideoSectionComponent, FooterComponent],
+  templateUrl: './app.component.html',
   styles: ''
 })
 export class AppComponent {
-  title = 'd-and-d-wiki';
+  usuario = 'Manuel';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        'hero-banner' : "url('/banner.jpg')",
+      }
+    },
   },
   plugins: [],
 }
